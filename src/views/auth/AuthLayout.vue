a4c38609f7aad22c33cb2d70d3a138dd1094c492
samuel <kurdzsamuel@gmail.com>
2024-03-06 08:52:02 +0100

    feat: invite UI

--- src/views/auth/AuthLayout.vue
+++ src/views/auth/AuthLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <img class="mr-2 size-6 rounded absolute left-5 top-5 lg:hidden" src="/apple-touch-icon.png" alt="logo"/>
     <Button
       variant="ghost"
       class="absolute right-4 top-4 md:right-8 md:top-8"
@@ -9,8 +10,8 @@
     <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
       <div class="absolute inset-0 bg-foreground" />
       <div class="z-20 flex items-center text-lg font-medium">
-        <Command class="mr-2 size-6 stroke-1" />
-        Acme Inc
+        <img class="mr-2 size-6 rounded" src="/apple-touch-icon.png" alt="logo"/>
+        Endavu
       </div>
       <div class="z-20 mt-auto">
         <blockquote class="space-y-2">
@@ -29,7 +30,7 @@
       <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <RouterView />
         <p class="px-8 text-center text-sm text-muted-foreground">
-          By clicking continue, you agree to our
+          By clicking the send button, you agree to our
           <span class="underline underline-offset-4 hover:text-primary">
             Terms of Service
           </span>
@@ -46,5 +47,4 @@
 
 <script setup lang="ts">
 import {Button} from "@ui/button"
-import {Command} from "lucide-vue-next"
 </script>
--- src/views/auth/Invite.vue
+++ src/views/auth/Invite.vue
@@ -30,7 +30,7 @@
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 1 1-6.219-8.56">
             </path>
           </svg>
-          Sign In with Email
+          Send request
         </Button>
       </div>
     </form>
